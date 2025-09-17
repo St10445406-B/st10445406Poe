582f492af33089b01500e73cff620ce19dec7e62
Bradley5647 <bappenah@gmail.com>
2025-09-17 19:14:58 +0200

    Add project files.

---
+++ CMCS/Controllers/ClaimController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMCS.Controllers
+{
+    public class ClaimController : Controller
+    {
+        // GET: ClaimController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        // GET: ClaimController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClaimController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClaimController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClaimController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClaimController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClaimController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClaimController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CMCS/Models/Claim.cs
@@ -0,0 +1,12 @@
+﻿namespace CMCS.Models
+{
+    public class Claim
+    {
+        public int Id { get; set; }
+        public string? ClaimNumber { get; set; }
+        public string? lecturer { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public string? Status { get; set; }
+    }
+}
